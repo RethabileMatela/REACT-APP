ac0130dd8832b2476c8d72466b923fe84d9763c3
RethabileMatela <matelarethabile@gmail.com>
2022-03-29 10:00:39 +0200

    search movies app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,15 @@
-import {useEffect, useState } from "react";
-import './App.css';
+import React, { useEffect } from "react";
+//a0d5c2e6
 
+const API_URL = 'http://www.ombdapi.com?apikey=a0d5c2e6'
 
+const App = () =>{
+const searchMovies = async(title)=>{
+const response = await fetch (`${API_URL}&s=${title}`)
+}
+useEffect(()=>{
 
-const App = () => {
-  const[counter, setCounter]= useState(0);
-
-  useEffect(()=>
-  {
-    setCounter(100);
-  },[])
-  return (
-    <div className="App">
-<button onClick = {() => setCounter((prevCount => prevCount-1))}>-</button>
-<h1>{ counter }</h1>
-<button  onClick = {() => setCounter((prevCount => prevCount+1))} >+</button>
-    </div>
-  );
+},[])
 }
 
 export default App;
