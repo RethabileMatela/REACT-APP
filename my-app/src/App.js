4bec6b354a004ac8e54ba4fbba85631f6d526c48
RethabileMatela <matelarethabile@gmail.com>
2022-03-29 11:42:24 +0200

    movie app completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
+
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = 'http://www.ombdapi.com?apikey=a0d5c2e6'
-
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
